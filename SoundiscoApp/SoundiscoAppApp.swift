bbaba93b80829560b5083e10ca8b51ccfdae7b87
Ramon-4949 <ramon_gonzalez2@ucne.edu.do>
2026-09-14 16:29:11 -0400

    Initial Commit

---
+++ SoundiscoApp/SoundiscoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundiscoAppApp.swift
+//  SoundiscoApp
+//
+//  Created by ramon.  on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundiscoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
